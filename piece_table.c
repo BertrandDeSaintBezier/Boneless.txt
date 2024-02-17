daf49fe85e2f313d4443f98f2a53480c6388c086
Léonard <epoc4oftime@gmail.com>
2024-02-17 15:13:52 +0100

    wip implementation of piece table

--- piece_table.c
+++ piece_table.c
@@ -1,66 +1,78 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_LINE_LENGTH 256
+#define KEYBOARD_BUFFER 70000000
 
-enum PieceSource {
-    Original,
-    Add
-};
+enum PieceSource { Original, Add };
 
-struct Piece{
+struct Piece;
+
+typedef struct Piece{
     int start;
-    int length;
-    enum PieceSource PIECE_SOURCE; 
-};
+    size_t len;
+    enum PieceSource PIECE_SOURCE;
+    struct Piece *next;
+    struct Piece *prev;
+} Piece_t;
 
-struct PieceTable{
-    char* original;
-    char* add;
-    struct Piece pieces[25];
-    size_t pieces_length;
-};
+typedef struct {
+    Piece_t *start;    
+    Piece_t *end;
+    size_t count;
+    size_t length;
+} PieceTable;
 
-char* compute_piece_string(struct Piece source, struct PieceTable* table){
-    char* result = (char*)malloc(source.length + 1);
-    if(source.PIECE_SOURCE == Original){
-        strncpy(result, table->original + source.start, source.length);
+PieceTable *init_piece_table(size_t length) {
+    Piece_t *start = (Piece_t *)malloc(sizeof(Piece_t));
+    start->start = 0;
+    start->len = length;
+    start->PIECE_SOURCE = Original;
+    start->next = NULL;
+    start->prev = NULL;
+
+    PieceTable *table = (PieceTable *)malloc(sizeof(PieceTable));
+    table->start = start;
+    table->end = NULL;
+    table->count = 1;
+    table->length = length;
+}
+
+char *print_table_contents(PieceTable *table, const char* original_sequence, const char* added_sequence) {
+    char *result = (char *)malloc(table->length);
+    int offset = 0;
+    Piece_t current = *(table->start);
+
+    for (size_t i = 0; i < table->count; ++i) {
+        if(current.PIECE_SOURCE == Original){
+            strncpy(result + offset, original_sequence + current.start, current.len);
+        }
+        else if(current.PIECE_SOURCE == Add){
+            strncpy(result + offset, added_sequence + current.start, current.len);
+        }
+
+        if(current.next != NULL){
+            current = *(current.next);
+            offset += current.len;
+        }
     }
-    else if (source.PIECE_SOURCE == Add){
-        strncpy(result, table->add + source.start, source.length);
-    }
-    result[source.length] = '\0';
+
     return result;
 }
 
-struct Piece make_piece(enum PieceSource source, int start, int length){
-    struct Piece result;
-    result.PIECE_SOURCE = source;
-    result.start = start;
-    result.length = length;
-    return result;
-}
+int main(int argc, char *argv[]) {
+    const char *original = "The brownfox.";
+    const char *added = "quick   jumped over the white fence";
 
-int main(char argc, char* argv[]){
-    struct PieceTable table;
-    table.original = "Thebrown fox.";
-    table.add =" quick  jumped over the white fence";
+    PieceTable *table = init_piece_table(13);
+    // simulate adding text from keyboard buffer, one piece at a time
+    // then print the result
+    // each time a piece is added, the output is three pieces : the original one (right), the new text from the keyboard buffer (middle), amd the end of the original piece (left)
+    
 
-    table.pieces[0] = make_piece(Original, 0, 3);
-    table.pieces[1] = make_piece(Add, 0, 7);
-    table.pieces[2] = make_piece(Original, 3, 9);
-    table.pieces[3] = make_piece(Add, 7, 28);
-    table.pieces[4] = make_piece(Original, 12, 1);
-    table.pieces_length = 5;
+    char* output = print_table_contents(table, original, added);
 
-    char line[MAX_LINE_LENGTH] = "";
+    printf("%s", output);
 
-    for(size_t i = 0; i < table.pieces_length; ++i){
-        strcat(line, compute_piece_string(table.pieces[i], &table));
-    }
-
-    printf("%s", line);
     return 0;
 }
